88a4b9a616ad7c687ff38fc0af2ea01a9033dadd
Daniel <daniel@leafcomputer.com.br>
2013-04-30 19:14:48 -0300

    Going back to legacy file upload and download mode

--- developer/EncryptFile.js
+++ developer/EncryptFile.js
@@ -2,16 +2,60 @@
 
 importScripts('sjcl.js');
 
+
+password = "";
+inputFile = "";
+outputFile = "";
+middleFile = "";
+Place = 0;
+toEncrypt = undefined;
+
 self.onmessage = function(event) {
 
+	if(event.data['cmd'] == "encrypt" || event.data['cmd'] == "decrypt")
+	{
+    password = event.data['password'] || undefined;
+
+    if (typeof(password) != "string")
+    {
+        Err("Password must be a string");
+    }
+    
 	if(event.data['cmd'] == "encrypt")
 	{
-	postMessage({'status': 'CryptChunk', 'data': sjcl.encrypt(event.data['password'], event.data['chunk'])})
-	}
-	else if(event.data['cmd'] == "decrypt")
-	{
-	postMessage({'status': 'Chunk', 'data': DecryptTheFile(event.data['password'], event.data['chunk'])})
-	}
+    	toEncrypt = true;
+    	MakeLog("Going to encrypt soon...");
+    }
+    else
+    {
+    	toEncrypt = false;
+    	MakeLog("Going to decrypt soon...");
+    }
+    postMessage({'status': 'more data'});
+    }
+    else if(event.data['cmd'] == "more data")
+    {
+    	SendChunk();
+    }
+    else if(event.data['cmd'] == "Chunk")
+    {
+    	if(event.data['data'] != "")
+    	{
+    		inputFile += event.data['data'];
+    		postMessage({'status': 'more data'});
+    	}
+    	else
+    	{
+    		if(toEncrypt)
+    		{
+    			EncryptTheFile();
+    		}
+    		else
+    		{
+    			DecryptTheFile();
+    		}
+    	}
+    }
 }
 
 function Err(e) {
@@ -22,8 +66,35 @@ function MakeLog(e) {
     postMessage({'status': 'debug', 'message':e});
 }
 
-function DecryptTheFile(password,inputFile) {
-var outputFile;
+
+function SendChunk()
+{
+MakeLog(typeof outputFile);
+if((typeof outputFile) == "string")
+{
+postMessage({'status': 'CryptChunk', 'data':outputFile.substring(Place*100000,(Place+1)*100000)});
+}
+else
+{
+postMessage({'status': 'CryptChunk', 'data':outputFile.subarray(Place*100000,(Place+1)*100000),'Place': Place});
+}
+Place++;
+}
+
+function EncryptTheFile() {
+    try {
+        MakeLog("starting to encrypt file...");
+        outputFile = sjcl.encrypt(password, inputFile);
+        MakeLog("encryption done!!");
+        postMessage({'status': 'begin', 'data':outputFile.length});
+    	MakeLog("File sent from worker to main script");
+    } catch(err) {
+        Err("Error on encryption: " + err.toString());
+        return true;
+    }
+}
+
+function DecryptTheFile() {
     try {
         MakeLog("starting to decrypt file...");
         middleFile = sjcl.decrypt(password, inputFile);
@@ -35,9 +106,10 @@ var outputFile;
 										outputFile[i] = middleFile.charCodeAt(i);
 									}
 		MakeLog("Encoded");
-	return outputFile;
+    	postMessage({'status': 'begin', 'data':outputFile.length,'size':outputFile.length});
+    	MakeLog("File sent from worker to main script");
     } catch(err) {
         Err("Error on decryption: " + err.toString());
-        return false;
+        return true;
     }
 }
